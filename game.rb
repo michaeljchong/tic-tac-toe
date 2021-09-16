2e8150713c91a8c374670f61fb5b2e4cc5be5dab
Michael Chong <mikec0920@gmail.com>
2021-09-16 15:26:04 -0700

    Initialized Board class

--- game.rb
+++ game.rb
@@ -1,3 +1,22 @@
 class Board
-  
+  attr_accessor :board
+
+  def initialize
+    @board = [
+      ['','',''],
+      ['','',''],
+      ['','','']
+    ]
+  end
+
+  def position(row, col)
+    @board[row][col]
+  end
 end
+
+class Player
+
+end
+
+b = Board.new()
+p b.board
