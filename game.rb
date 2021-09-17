bab76b13f7090839b0c2c558631731e58095791f
Michael Chong <mikec0920@gmail.com>
2021-09-17 09:54:45 -0700

    Fixed win? method

--- game.rb
+++ game.rb
@@ -43,9 +43,7 @@ class Board
   end
 
   def win?(sign)
-    check_rows(sign)
-    check_cols(sign)
-    check_diags(sign)
+    check_rows(sign) || check_cols(sign) || check_diags(sign)
   end
 
   def full?
@@ -62,11 +60,29 @@ class Board
 end
 
 class Player
-  attr_reader :name, :sign
+  attr_accessor :name
+  attr_reader :sign
 
   def initialize(sign)
     @sign = sign
     @name = (@sign == 'X') ? "Player 1" : "Player 2"
+  end
+
+  def check_name(name)
+    
+  end
+
+  def turn(board)
+    print "#{@name}, what is your next move? (Enter a number from 1-9) "
+    while
+      move = self.get_move
+      row, col = move[0], move[1]
+      if board.position(row, col) == ' '
+        board.set_value(row, col, @sign)
+        break
+      end
+      puts "That space has already been taken. Please choose another one."
+    end
   end
 
   def get_move
@@ -92,39 +108,38 @@ class Game
     @player_2 = Player.new('O')
   end
 
-  def turn(player)
-    puts "#{player.name}, what is your next move? Enter a number from 1-9."
-    while
-      move = player.get_move
-      row, col = move[0], move[1]
-      if @board.position(row, col) == ' '
-        @board.set_value(row, col, player.sign)
-        break
-      end
-      puts "That space has already been taken. Please choose another one."
-    end
-  end
-
   def intro
     puts "Let's play Tic Tac Toe!"
+    self.set_names
     puts "Enter your moves according to the following grid"
     @board.display
     @board.clear
     puts "Let's begin..."
   end
 
+  def set_names
+    print "Who is playing? (default: Player 1) "
+    name = gets.chomp
+    @player_1.name = name unless name.empty?
+    print "Who else is playing? (default: Player 2) "
+    name = gets.chomp
+    @player_2.name = name unless name.empty?
+  end
+
   def play
     self.intro
-    current_player = @player_1
+    player = @player_1
     until @board.full?
       @board.display
-      turn(current_player)
-      if @board.win?(current_player.sign)
-        puts "#{current_player.name} has won!"
+      player.turn(@board)
+      if @board.win?(player.sign)
+        @board.display
+        puts "#{player.name} has won!"
         return
       end
-      current_player = (current_player == @player_1) ? @player_2 : @player_1
+      player = (player == @player_1) ? @player_2 : @player_1
     end
+    @board.display
     puts "It's a draw!"
   end
 end
