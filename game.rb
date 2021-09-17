b05d5f1737e25d7d75210fdec0f21cc0cc5ebc10
Michael Chong <mikec0920@gmail.com>
2021-09-16 17:31:10 -0700

    Completed display and full methods

--- game.rb
+++ game.rb
@@ -1,16 +1,35 @@
 class Board
-  attr_accessor :board
+  attr_reader :board
 
   def initialize
     @board = [
-      ['','',''],
-      ['','',''],
-      ['','','']
+      ['x','x','x'],
+      ['x','x','x'],
+      ['x','x','x']
     ]
   end
 
+  def display
+    board.each do |row|
+      print "|"
+      row.each { |value| print "#{value}|" }
+      puts
+      puts " - - - "
+    end
+  end
+
   def position(row, col)
-    @board[row][col]
+    board[row][col]
+  end
+
+  def set_value(row, col, value)
+    board[row][col] = value
+  end
+
+  def full?
+    board.none? do |row|
+      row.include?('')
+    end
   end
 end
 
@@ -19,4 +38,7 @@ class Player
 end
 
 b = Board.new()
-p b.board
+b.display
+b.set_value(0,1,'2')
+b.display
+p b.full?
